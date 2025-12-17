ca3a2c7f73418d0415f5440399342c9651e72301
Pravallika Tammineni <pravallikaat@gmail.com>
2025-12-17 21:53:09 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -13,6 +13,16 @@ def home():
 def status():
     return "AISA project is active and learning in progress"
 
+def classify_intent(user_issue):
+     if "vpn" in user_issue or "network" in user_issue:
+       return "Network issue"
+    elif "password" in user_issue or "signin" in user_issue or "login" in user_issue or "signup" in user_issue:
+        return "Access issue"
+    elif "laptop" in user_issue or "computer" in user_issue or "slow" in user_issue:
+        return "Hardware issue"
+    else:
+        "General IT issue"
+
 @app.route("/issue", methods=["POST"])
 def issue():
     global ticket_id
@@ -20,17 +30,15 @@ def issue():
     data=request.json
     user_issue = data["issue"].lower()
 
-    if "vpn" in user_issue or "network" in user_issue:
-        category="Network issue"
+    category= classify_intent(user_issue)
+
+    if category=="Network issue":
         solution="Please check your internet connection and restart the VPN."
-    elif "password" in user_issue or "signin" in user_issue or "login" in user_issue or "signup" in user_issue:
-        category="Access issue"
+    elif category=="Access issue":
         solution="Please try resetting your password or contact IT support."
-    elif "laptop" in user_issue or "computer" in user_issue or "slow" in user_issue:
-        category="Hardware issue"
+    elif category=="Hardware issue":
         solution="Please restart your laptop and close unused applications."
     else:
-        category="General IT issue"
         solution="Your issue has been noted. IT support will contact you."
 
     ticket={
